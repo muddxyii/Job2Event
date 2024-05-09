2c1e28ebaa4aa2e23122670e72e6d26942ccdc3b
mudd <muddxyii@gmail.com>
2024-05-09 09:43:27 -0700

    rust dependencies

---
+++ job_to_event/target/debug/deps/job_to_event-43f512387d10d96a.d
@@ -0,0 +1,5 @@
+/home/muddxyii/Downloads/Job2Event/job_to_event/target/debug/deps/libjob_to_event-43f512387d10d96a.rmeta: src/main.rs
+
+/home/muddxyii/Downloads/Job2Event/job_to_event/target/debug/deps/job_to_event-43f512387d10d96a.d: src/main.rs
+
+src/main.rs:
