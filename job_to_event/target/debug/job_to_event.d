86fb958194ee9a8ee7fff4f1b0d0523741382d8c
Nikolas <npadi43@wgu.edu>
2024-05-09 17:25:39 -0700

    fixed gitignore

--- job_to_event/target/debug/job_to_event.d
+++ job_to_event/target/debug/job_to_event.d
@@ -1,1 +1,1 @@
-/home/muddxyii/Downloads/Job2Event/job_to_event/target/debug/job_to_event: /home/muddxyii/Downloads/Job2Event/job_to_event/src/main.rs
+C:\Users\A47P\Downloads\python_test\job_to_event\target\debug\job_to_event.exe: C:\Users\A47P\Downloads\python_test\job_to_event\src\main.rs
