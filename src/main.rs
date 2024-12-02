cfc86843c458595817f3a1b270a77e2297133d7e
muddxyii <muddxyii@gmail.com>
2024-12-02 12:42:24 -0700

    Handles multiple job2event instances

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,13 @@
 use calamine::{open_workbook_auto, Data, Reader};
 use std::env;
-
+use std::fmt::{Display, Formatter};
+use std::net::TcpListener;
 use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
 use actix_files as fs;
 use webbrowser;
-
+use tokio::time::sleep;
+use std::time::Duration;
+use sysinfo::{System};
 use serde::Serialize;
 
 const DATE_CONTACTED: usize = 0;
@@ -31,8 +34,6 @@ const PARTS: usize = 20;
 const NOTES: usize = 21;
 const CALENDAR_ENTRY: usize = 22;
 
-
-
 #[derive(Serialize)]
 pub struct CalendarEvent {
     pub title: String,
@@ -40,8 +41,18 @@ pub struct CalendarEvent {
     pub address: String,
 }
 
+impl Display for CalendarEvent {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "  Title: {}\n  Address: {}\n  Description:\n{}",
+               if self.title.is_empty() { "No Title Found" } else { &self.title },
+               if self.address.is_empty() { "No Address Found" } else { &self.address },
+               if self.description.is_empty() { "  No Description Found" } else { &self.description })
+    }
+}
+
 #[get("/api/event-data")]
 async fn get_event_data() -> impl Responder {
+    println!("Attempting to load event data from Excel");
     let args: Vec<String> = env::args().collect();
     let event = if args.len() >= 3 {
         let file_path = &args[1];
@@ -51,25 +62,29 @@ async fn get_event_data() -> impl Responder {
         let sheet = workbook.worksheet_range_at(0).unwrap().unwrap();
 
         if let Some(row) = sheet.rows().nth(row_num - 1) {
+            println!("  Found row {} in sheet", row_num);
             CalendarEvent {
                 title: row[CALENDAR_ENTRY].to_string(),
-                description: format_details(row),
-                address: row[ADDRESS].to_string()
+                address: row[ADDRESS].to_string(),
+                description: format_details(row)
             }
         } else {
+            println!("  No data found in file");
             CalendarEvent {
                 title: "Default Event".to_string(),
-                description: "No data found".to_string(),
-                address: "".to_string()
+                address: "".to_string(),
+                description: "No data found".to_string()
             }
         }
     } else {
+        println!("  No file provided");
         CalendarEvent {
-            title: "Default Event".to_string(),
-            description: "No file provided".to_string(),
-            address: "".to_string()
+            title: "".to_string(),
+            address: "".to_string(),
+            description: "".to_string()
         }
     };
+    println!("Data Scraped:\n{}", event.to_string());
 
     web::Json(event)
 }
@@ -81,8 +96,30 @@ async fn index() -> impl Responder {
         .body(include_str!("../static/index.html"))
 }
 
+fn kill_other_instances() {
+    let s = System::new_all();
+    let current_pid = std::process::id();
+
+    for (pid, process) in s.processes() {
+        if process.name() == "job2event.exe" && pid.as_u32() != current_pid {
+            println!("Closed Process ID: {}", pid.as_u32());
+            process.kill();
+        }
+    }
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    // Clear previous instances of Job2Event
+    kill_other_instances();
+    if TcpListener::bind("127.0.0.1:8080").is_err() {
+        println!("Port 8080 is in use. Waiting for other instance to close...");
+        sleep(Duration::from_secs(2)).await;
+    } else {
+        println!("Port 8080 is not in use. Creating new instance...");
+    }
+
+    // Create new instance of Job2Event
     let server = HttpServer::new(|| {
         App::new()
             .service(index)
@@ -91,6 +128,7 @@ async fn main() -> std::io::Result<()> {
     })
         .bind("127.0.0.1:8080")?;
 
+    println!("Created new instance at http://localhost:8080/");
     webbrowser::open("http://localhost:8080/").expect("Failed to open URL");
 
     server.run().await
